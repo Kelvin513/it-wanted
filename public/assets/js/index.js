9ad269115a782ecae705a503bbfd17562c18e6e4
Adele Yu <fhead17@gmail.com>
2016-07-27 14:53:41 +0800

    Add cmd+[ and cmd+] support

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -13,6 +13,17 @@ sr.reveal('.sec3 .reveal',400);
 sr.reveal('.sec4 .reveal',400);
 sr.reveal('.sec5 .reveal',340);
 
+onkeydown = function(e){
+  if(((e.metaKey || e.ctrlKey) || e.ctrlKey) && e.keyCode == 219){
+    e.preventDefault();
+    console.log("prev tab");
+    // $(".nav.active").removeClass("active").next(".nav").addClass("active");
+  }
+  if(((e.metaKey || e.ctrlKey) || e.ctrlKey) && e.keyCode == 221){
+    e.preventDefault();
+    console.log("next tab");
+  }
+}
 
 
 
